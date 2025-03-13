e1e286f5f479ed09db2c4060f10cd7b3ce86b891
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-03-13 09:42:28 +0100

    Custom Vec3 + moving line with mouse

--- lab1.cpp
+++ lab1.cpp
@@ -1,9 +1,5 @@
-//=============================================================================================
-// Z�ld h�romsz�g: A framework.h oszt�lyait felhaszn�l� megold�s
-//=============================================================================================
 #include "framework.h"
 
-// cs�cspont �rnyal�
 const char* vertSource = R"(
 	#version 330				
     precision highp float;
@@ -15,7 +11,6 @@ const char* vertSource = R"(
 	}
 )";
 
-// pixel �rnyal�
 const char* fragSource = R"(
 	#version 330
     precision highp float;
@@ -30,55 +25,120 @@ const char* fragSource = R"(
 
 const int winWidth = 600, winHeight = 600;
 
+class Vec3 {
+public:
+    union {
+        float x;
+        float r;
+    };
+    union {
+        float y;
+        float g;
+    };
+    union {
+        float z;
+        float b;
+    };
+    Vec3(float f1, float f2, float f3) : x(f1), y(f2), z(f3) {}
+    Vec3(float f1, float f2) : x(f1), y(f2), z(1.0f) {}
+    Vec3(float f1) : x(f1), y(f1), z(f1) {}
+    Vec3() : x(0), y(0), z(0) {}
+    Vec3(const Vec3& o) : x(o.x), y(o.y), z(o.z) {}
+
+    float len2() {
+        return x * x + y * y;
+    }
+
+    Vec3 operator+(const Vec3& o) const {
+        return Vec3(x + o.x, y + o.y, z + o.z);
+    }
+    Vec3 operator-(const Vec3& o) const {
+        return Vec3(x - o.x, y - o.y, z - o.z);
+    }
+    Vec3 operator+=(const Vec3& o) {
+        x += o.x, y += o.y, z += o.z;
+        return *this;
+    }
+    Vec3 operator-=(const Vec3& o) {
+        x -= o.x, y -= o.y, z -= o.z;
+        return *this;
+    }
+    Vec3 operator*(const float& s) const {
+        return Vec3(x * s, y * s, z * s);
+    }
+    Vec3 operator/(const float& s) const {
+        return Vec3(x / s, y / s, z / s);
+    }
+    operator vec3() {
+        return vec3(x, y, z);
+    }
+    Vec3 windowToViewSpace() {
+        return Vec3((((float)x / winWidth) * 2.0f) - 1.0f,
+                    (((float)(winHeight - y) / winHeight) * 2.0f) - 1.0f, 1.0f);
+    }
+};
+
 enum class InputMode {
     AddPoint,
     AddLine,
     MoveLine,
     AddIntersectionPoint,
 };
-float len2(vec3 a, vec3 b) {
-    return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
-}
 
-void getLineEndsFromTwoPoints(vec3 p1, vec3 p2, vec3& op1, vec3& op2) {
-    vec3 d = p1 - p2, o = d / (sqrt(d.x * d.x + d.y * d.y)) * 3.0f;
+void getLineEndsFromTwoPoints(Vec3 p1, Vec3 p2, Vec3& op1, Vec3& op2) {
+    Vec3 d = p1 - p2, o = d / (sqrt(d.x * d.x + d.y * d.y)) * 10.0f;
     op1 = p1 + o, op2 = p1 - o;
     // we get the direction vector, normalize it, multiply it by 3 so it will cover the largest
     // possible padding needed (diagonally across the screen), and add that offset in both ways to
     // the first vector
 }
+Vec3 getLineIntersection(Vec3 l1p1, Vec3 l1p2, Vec3 l2p1, Vec3 l2p2) {
+
+}
 
 class GreenTriangleApp : public glApp {
-    Geometry<vec3>* pointList; // geometria
-    Geometry<vec3>* lineList;  // geometria
-    vec3 lineTempPoint;
-    GPUProgram* gpuProgram; // cs�cspont �s pixel �rnyal�k
+    Geometry<Vec3>* pointList;
+    Geometry<Vec3>* lineList;
+    Vec3 lineTempPoint;
+    GPUProgram* gpuProgram;
     InputMode mode;
+    bool isMovingLine = false;
+    Vec3 movingLineOrigin, movingLineCurrent;
+    int movingLineIndex;
 
 public:
     GreenTriangleApp() : glApp("Homework 1") {}
 
     // Inicializ�ci�,
     void onInitialization() {
-        pointList = new Geometry<vec3>;
-        lineList = new Geometry<vec3>;
+        pointList = new Geometry<Vec3>;
+        lineList = new Geometry<Vec3>;
         pointList->Vtx() = {};
         lineList->Vtx() = {};
-        lineTempPoint = vec3(0.0f, 0.0f, -1.0f);
+        lineTempPoint = Vec3(0.0f, 0.0f, -1.0f);
         pointList->updateGPU();
         gpuProgram = new GPUProgram(vertSource, fragSource);
         glPointSize(10.0f);
         glLineWidth(3.0f);
     }
 
-    // Ablak �jrarajzol�s
     void onDisplay() {
-        glClearColor(0.125f, 0.125f, 0.125f, 0); // h�tt�r sz�n
-        glClear(GL_COLOR_BUFFER_BIT);            // rasztert�r t�rl�s
+        glClearColor(0.125f, 0.125f, 0.125f, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
         glViewport(0, 0, winWidth, winHeight);
 
+        Vec3 deltaLineMovement =
+            ((movingLineCurrent - movingLineOrigin));
+        if (isMovingLine) {
+            lineList->Vtx()[movingLineIndex * 2] += deltaLineMovement;
+            lineList->Vtx()[movingLineIndex * 2 + 1] += deltaLineMovement;
+        }
         lineList->updateGPU();
         lineList->Draw(gpuProgram, GL_LINES, vec3(0.0f, 1.0f, 1.0f));
+        if (isMovingLine) {
+            lineList->Vtx()[movingLineIndex * 2] -= deltaLineMovement;
+            lineList->Vtx()[movingLineIndex * 2 + 1] -= deltaLineMovement;
+        }
         pointList->updateGPU();
         pointList->Draw(gpuProgram, GL_POINTS, vec3(1.0f, 0.0f, 0.0f));
     }
@@ -87,9 +147,8 @@ public:
         if (but != MOUSE_LEFT) {
             return;
         }
-        vec3 mouseCoordsToViewCoords =
-            vec3((((float)pX / winWidth) * 2.0f) - 1.0f,
-                 (((float)(winHeight - pY) / winHeight) * 2.0f) - 1.0f, 1.0f);
+        Vec3 mouseCoordsToViewCoords = Vec3(pX, pY, 1.0f).windowToViewSpace();
+
         switch (mode) {
         case InputMode::AddPoint:
             pointList->Vtx().push_back(mouseCoordsToViewCoords);
@@ -97,11 +156,11 @@ public:
             printf("Added point\n");
             break;
         case InputMode::AddLine: {
-            vec3 selectedPoint = vec3(0.0f, 0.0f, -1.0f);
+            Vec3 selectedPoint = Vec3(0.0f, 0.0f, -1.0f);
             for (auto& pt : pointList->Vtx()) {
                 // printf("pt: %f %f %f\n", pt.x, pt.y, pt.z);
                 // printf("dist: %f\n", len2(mouseCoordsToViewCoords, pt));
-                if (len2(mouseCoordsToViewCoords, pt) < 0.005f) {
+                if (Vec3(mouseCoordsToViewCoords - pt).len2() < 0.005f) {
                     // printf("Clicked in an epsilon range of a point\n");
                     selectedPoint = pt;
                     break;
@@ -113,7 +172,7 @@ public:
                 lineTempPoint = selectedPoint;
                 printf("Selected first point of line\n");
             } else {
-                vec3 line1(1), line2(1);
+                Vec3 line1(1), line2(1);
                 getLineEndsFromTwoPoints(selectedPoint, lineTempPoint, line1, line2);
                 lineList->Vtx().push_back(line1);
                 lineList->Vtx().push_back(line2);
@@ -121,7 +180,14 @@ public:
                 lineTempPoint.z = -1.0f;
                 printf("Added line\n");
             }
+            break;
         }
+        case InputMode::MoveLine:
+            isMovingLine = true;
+            movingLineOrigin = Vec3(pX, pY).windowToViewSpace();
+            movingLineCurrent = movingLineOrigin;
+            movingLineIndex = 0;
+            break;
         default:
             break;
         }
@@ -131,7 +197,6 @@ public:
         switch (key) {
         case ' ':
             exit(0);
-            break;
         case 'p':
             mode = InputMode::AddPoint;
             printf("mode = AddPoint\n");
@@ -151,6 +216,25 @@ public:
         default:
             break;
         }
+    }
+
+    virtual void onMouseReleased(MouseButton but, int pX, int pY) {
+        if (!isMovingLine) {
+            return;
+        }
+        isMovingLine = false;
+        Vec3 deltaLineMovement =
+            (movingLineCurrent - movingLineOrigin);
+        lineList->Vtx()[movingLineIndex * 2] += deltaLineMovement;
+        lineList->Vtx()[movingLineIndex * 2 + 1] += deltaLineMovement;
+        refreshScreen();
+    }
+    virtual void onMouseMotion(int pX, int pY) {
+        if (!isMovingLine) {
+            return;
+        }
+        movingLineCurrent = Vec3(pX, pY).windowToViewSpace();
+        refreshScreen();
     }
 };
 
