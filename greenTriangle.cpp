9404b3414535022ddd743bcb9bf97640f283735a
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-03-16 13:17:18 +0100

    clang

--- greenTriangle.cpp
+++ greenTriangle.cpp
@@ -4,7 +4,7 @@
 #include "framework.h"
 
 // csúcspont árnyaló
-const char * vertSource = R"(
+const char* vertSource = R"(
 	#version 330				
     precision highp float;
 
@@ -16,7 +16,7 @@ const char * vertSource = R"(
 )";
 
 // pixel árnyaló
-const char * fragSource = R"(
+const char* fragSource = R"(
 	#version 330
     precision highp float;
 
@@ -31,27 +31,26 @@ const char * fragSource = R"(
 const int winWidth = 600, winHeight = 600;
 
 class GreenTriangleApp : public glApp {
-	Geometry<vec2>* triangle;  // geometria
-	GPUProgram* gpuProgram;	   // csúcspont és pixel árnyalók
+    Geometry<vec2>* triangle; // geometria
+    GPUProgram* gpuProgram;   // csúcspont és pixel árnyalók
 public:
-	GreenTriangleApp() : glApp("Green triangle") { }
+    GreenTriangleApp() : glApp("Green triangle") {}
 
-	// Inicializáció, 
-	void onInitialization() {
-		triangle = new Geometry<vec2>;
-		triangle->Vtx() = { vec2(-0.8f, -0.8f), vec2(-0.6f, 1.0f), vec2(0.8f, -0.2f) };
-		triangle->updateGPU();
-		gpuProgram = new GPUProgram(vertSource, fragSource);
-	}
+    // Inicializáció,
+    void onInitialization() {
+        triangle = new Geometry<vec2>;
+        triangle->Vtx() = {vec2(-0.8f, -0.8f), vec2(-0.6f, 1.0f), vec2(0.8f, -0.2f)};
+        triangle->updateGPU();
+        gpuProgram = new GPUProgram(vertSource, fragSource);
+    }
 
-	// Ablak újrarajzolás
-	void onDisplay() {
-		glClearColor(0, 0, 0, 0);     // háttér szín
-		glClear(GL_COLOR_BUFFER_BIT); // rasztertár törlés
-		glViewport(0, 0, winWidth, winHeight);
-		triangle->Draw(gpuProgram, GL_TRIANGLES, vec3(0.0f, 1.0f, 0.0f));
-	}
+    // Ablak újrarajzolás
+    void onDisplay() {
+        glClearColor(0, 0, 0, 0);     // háttér szín
+        glClear(GL_COLOR_BUFFER_BIT); // rasztertár törlés
+        glViewport(0, 0, winWidth, winHeight);
+        triangle->Draw(gpuProgram, GL_TRIANGLES, vec3(0.0f, 1.0f, 0.0f));
+    }
 };
 
 GreenTriangleApp app;
-
